23f7fa72ae00959f0f6a0f1905ae3378a53e39ff
Aloïs DENIEL <alois.deniel@gmail.com>
2025-08-24 15:25:40 +0200

    Fading image.

--- example/lib/omarchy_preview.dart
+++ example/lib/omarchy_preview.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:example/main.dart';
 import 'package:flutter_omarchy/flutter_omarchy.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -46,19 +48,27 @@ class _OmarchyPreviewState extends State<OmarchyPreview> {
               onNextTheme: _nextTheme,
             ),
             Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(image: wallpaper, fit: BoxFit.cover),
-                ),
-                padding:
-                    const EdgeInsets.all(14.0) +
-                    EdgeInsets.only(bottom: insets.bottom),
-                child: OmarchyWindow(
-                  child: IndexedStack(
-                    index: app.index,
-                    children: [for (final app in App.values) app.build()],
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: FadeInImage(
+                      placeholder: MemoryImage(kTransparentImage),
+                      image: wallpaper,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.all(14.0) +
+                        EdgeInsets.only(bottom: insets.bottom),
+                    child: OmarchyWindow(
+                      child: IndexedStack(
+                        index: app.index,
+                        children: [for (final app in App.values) app.build()],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
@@ -166,3 +176,73 @@ class OmarchyWindow extends StatelessWidget {
     );
   }
 }
+
+final kTransparentImage = Uint8List.fromList(<int>[
+  0x89,
+  0x50,
+  0x4E,
+  0x47,
+  0x0D,
+  0x0A,
+  0x1A,
+  0x0A,
+  0x00,
+  0x00,
+  0x00,
+  0x0D,
+  0x49,
+  0x48,
+  0x44,
+  0x52,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x08,
+  0x06,
+  0x00,
+  0x00,
+  0x00,
+  0x1F,
+  0x15,
+  0xC4,
+  0x89,
+  0x00,
+  0x00,
+  0x00,
+  0x0A,
+  0x49,
+  0x44,
+  0x41,
+  0x54,
+  0x78,
+  0x9C,
+  0x63,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x05,
+  0x00,
+  0x01,
+  0x0D,
+  0x0A,
+  0x2D,
+  0xB4,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x49,
+  0x45,
+  0x4E,
+  0x44,
+  0xAE,
+  0x42,
+  0x60,
+  0x82,
+]);
